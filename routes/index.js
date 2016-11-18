ddd534cf2dc559bdcf64b2607f795e93d20a5586
Anh1309 <ngocanh12t3bkdn@gmail.com>
2016-11-18 11:11:08 +0700

    UI v1

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/student-list', function(req, res, next){
+    res.render('student-list');
+});
+router.post('/uploadFile', function(req, res, next){
+    res.redirect('/student-list');
+})
 
 module.exports = router;
