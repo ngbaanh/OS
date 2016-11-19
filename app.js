37ea37bb1b8f8dcd83bbc609e8196de22e307986
Anh1309 <ngocanh12t3bkdn@gmail.com>
2016-11-19 15:27:56 +0700

    add student

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 // routing controllers
 app.get('/', SiteController.homepage);
+app.get('/add-student', SiteController.add_student);
+app.post('/add-student', SiteController.send_student_info);
 app.get('/video-api/upload', ApiController.video_api_upload);
 app.get('/video-api/wait-for-result', ApiController.video_api_wait_for_result);
 app.get('/video-api/test', ApiController.sample_detection);
--- web/controllers/SiteController.js
+++ web/controllers/SiteController.js
@@ -1,11 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/OS');
+var Schema = mongoose.Schema;
+
+var StudentSchema = new Schema({
+    student_ID: {
+        type: String,
+        required: true
+    },
+    student_name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    class: {
+        type: String,
+        required: true
+    },
+    personId: {
+        type: String,
+        required: true
+    }
+}, {collection: 'student'});
+var Student = mongoose.model('Student', StudentSchema);
+
 module.exports = {
-	homepage: function(req, res, next) {
-	  res.render('site/index', { title: 'Homepage' });
-	},
-	student_list: function(req, res, next){
-	    res.render('site/student-list');
-	},
-	upload_file: function(req, res, next){
-	    res.redirect('/student-list');
-	}
+    homepage: function(req, res, next) {
+      res.render('site/index', { title: 'Homepage' });
+    },
+    student_list: function(req, res, next){
+        res.render('site/student-list');
+    },
+    upload_file: function(req, res, next){
+        res.redirect('/student-list');
+    },
+    add_student: function(req, res, next){
+        res.render('site/add-student');
+    },
+    send_student_info: function(req, res, next){
+        var student = new Student(req.body);
+        student.save(function(err, student){
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/add-student');
+        });
+    }
 };
