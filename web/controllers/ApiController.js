a4b5a20061f9e9034f3c878c60ed25cce7347e29
nathanTran183 <phuctd1803@gmail.com>
2016-11-20 17:25:55 +0700

    problem with error 429
    Nho Quang xem giup cho ni

--- web/controllers/ApiController.js
+++ web/controllers/ApiController.js
@@ -2,7 +2,7 @@
 var subKey = '6d4c2daabe474357aa15ebf5df19b57c'; // for fast test only, please keep it in DB instead
 var regex = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?");
 var apiPassword = '7b6c2586a7a64ea9497bf2ccd094f71b8d0423dfd815a4978b00db5092977d38'; //Wistia API access Token
-
+var Student = require('../models/Student');
 module.exports = {
 	/* @author: ngbaanh */
 	/* Show upload form with subscription key inside */
@@ -48,9 +48,17 @@ module.exports = {
 	/* @author: phuc */
 	/* test compare face with face using face-api */
 	comparePersonFace: function(req, res, next){
-		var key = '70a5f8d52d2d4d34909ddf5f3624782c';	
-		var listImage = [];
-		listImage.push("https://scontent-hkg3-1.xx.fbcdn.net/t31.0-8/13909186_1130987973635466_983780621913768657_o.jpg");
-		res.render('face-api/comparePersonFace',{key:key,listImage:listImage});
+		var key = '70a5f8d52d2d4d34909ddf5f3624782c';
+		var listImage = [];		
+		//listImage.push("https://scontent-hkg3-1.xx.fbcdn.net/t31.0-8/14086255_1230246773672430_8102430862113403910_o.jpg");
+		listImage.push("https://s21.postimg.org/vq4jz79tz/15102150_935615266582396_1132777037_o.jpg");
+		//listImage.push("https://s22.postimg.org/hulmqbhb5/15127402_935615743249015_322149103_o.jpg");
+		Student.find({}).exec(function(err,students){
+			if (err) {
+				return next(err);
+			}
+
+			res.render('face-api/comparePersonFace',{key:key,listImage:listImage, students:students});
+		});
 	}
 };
